8c146d6ceb33830dd4b03f979db7661077d18603
jiangege47 <jiangege47@gmail.com>
2025-05-12 03:57:20 +0800

    remove unused codes

--- src/fsrs/types.ts
+++ src/fsrs/types.ts
@@ -4,26 +4,3 @@ export enum Rating {
   Good = 3,
   Easy = 4,
 }
-
-export interface ReviewLog {
-  id: string;
-  reviewTime: number;
-  rating: Rating;
-  state: CardState;
-  duration?: number;
-}
-
-export interface Card {
-  id: string;
-  due: number | null;
-  state: CardState;
-  difficulty: number;
-  stability: number;
-  elapsedDays: number;
-  scheduledDays: number;
-  lastReview: number | null;
-  currentRetention: number;
-  desiredRetention: number;
-  postpones: number;
-  reviewLogs: ReviewLog[];
-}
